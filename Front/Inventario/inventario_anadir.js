b4b39f1dc220f3533c4228d6d269cffd508d2f62
Sofia Herrera <76921444+SosofiaHerrera@users.noreply.github.com>
2024-05-15 21:01:40 -0600

    Add files via upload

---
+++ Front/Inventario/inventario_anadir.js
@@ -0,0 +1,82 @@
+var cookies = document.cookie.split(';');
+var cookie = null;
+
+for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if (cookie.startsWith("token=")) {
+        cookie = cookie.substring("token=".length);
+        break;
+    }
+}
+
+console.log(cookie);
+document.addEventListener("DOMContentLoaded", function() {
+    cargarCategorias();
+
+    // Asegúrate de que el elemento con el id "submit" esté presente en el documento HTML
+    document.getElementById("botonGuardar").addEventListener('click', agregarProducto);
+});
+async function agregarProducto() {
+    const nombre = document.getElementById("nombre").value;
+    const precio = document.getElementById("precio").value;
+    const stock = document.getElementById("cantidad").value;
+    const categoria = document.getElementById("categoria").value;
+    const descripcion = document.getElementById("descripcion").value;
+    const imagenInput = document.getElementById("imagen");
+    const imagen = imagenInput.files[0]; // Obtener el primer archivo seleccionado
+    const material = document.getElementById("material").value;
+    // Crear un objeto FormData para enviar los datos del formulario, incluida la imagen
+    var formData = new FormData();
+    formData.append('nombre', nombre);
+    formData.append('precio', precio);
+    formData.append('cantidad', stock);
+    formData.append('categoria', categoria);
+    formData.append('descripcion', descripcion);
+    formData.append('material', material);
+    formData.append('imagen', imagen);
+    try {
+        const response = await fetch("http://127.0.0.1:8000/productos/anadir", {
+            method: "POST",
+            headers: {
+                'Authorization': 'Token ' + cookie,
+            },
+            body: formData,
+        });
+        if (response.ok) {
+            alert("Producto agregado exitosamente.");
+            //location.href = "inventario.html";
+        } else {
+            throw new Error("Error al agregar el producto.");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+    
+}
+async function cargarCategorias() {
+    try {
+    const response = await fetch("http://127.0.0.1:8000/productos/categorias", {
+        method: "GET",
+        headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Token " + cookie,
+        },
+    });
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        categoria=data;
+        select=document.getElementById("categoria");
+        categoria.forEach(async (categoria) => {
+            let option = document.createElement("option");
+            option.text = categoria.nombre;
+            option.value = categoria.id;
+            await select.appendChild(option);
+        });
+    } else {
+        throw new Error("No se pudo obtener la lista de categorías.");
+    }
+    } catch (error) {
+    console.error("Error al cargar las categorías:", error);
+    }
+}
---
+++ Front/Inventario/inventario_editar.js
@@ -0,0 +1,121 @@
+var cookies = document.cookie.split(';');
+var cookie = null;
+
+for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if (cookie.startsWith("token=")) {
+        cookie = cookie.substring("token=".length);
+        break;
+    }
+}
+
+console.log(cookie);
+document.addEventListener("DOMContentLoaded", function() {
+    cargarCategorias();
+    obtenerProductos();
+    
+    
+    document.getElementById("botonGuardar").addEventListener('click', actualizarProducto);
+    const nuevaCategoriaCheckbox = document.getElementById("nuevaCategoriaCheckbox");
+    const nuevaCategoriaInput = document.getElementById("nuevaCategoriaInput");
+    
+    nuevaCategoriaCheckbox.addEventListener("change", function() {
+        nuevaCategoriaInput.disabled = !nuevaCategoriaCheckbox.checked;
+        nuevaCategoriaInput.value = ""; 
+    });
+});
+    
+async function cargarCategorias() {
+    try {
+        const response = await fetch("http://127.0.0.1:8000/productos/categorias", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Token " + cookie,
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            const categoriaSelect = document.getElementById("categoriaSelect");
+            
+            data.forEach(categoria => {
+                const option = document.createElement("option");
+                option.text = categoria.nombre;
+                option.value = categoria.id;
+                categoriaSelect.appendChild(option);
+            });
+        } else {
+            throw new Error("No se pudo obtener la lista de categorías.");
+        }
+    } catch (error) {
+        console.error("Error al cargar las categorías:", error);
+    }
+}
+async function actualizarProducto() {
+    const id = producto.id;
+    const nombre = document.getElementById("nombre").value;
+    const precio = document.getElementById("precio").value;
+    const stock = document.getElementById("cantidad").value;
+    const categoria = document.getElementById("categoriaSelect").value;
+    const descripcion = document.getElementById("descripcion").value;
+    const imagenInput = document.getElementById("imagen");
+    const imagen = imagenInput.files[0]; // Obtener el primer archivo seleccionado
+    const material = document.getElementById("material").value;
+    // Crear un objeto FormData para enviar los datos del formulario, incluida la imagen
+    const formData = new FormData();
+    formData.append('id', id);
+    formData.append('nombre', nombre);
+    formData.append('precio', precio);
+    formData.append('cantidad', stock);
+    formData.append('categoria', categoria);
+    formData.append('descripcion', descripcion);
+    formData.append('material', material);
+    formData.append('imagen', imagen);
+    try {
+        const response = await fetch("http://127.0.0.1:8000/productos/editar", {
+            method: "PUT", // Cambiado de PUT a POST
+            headers: {
+                'Authorization': 'Token ' + cookie,
+            },
+            body: formData,
+        });
+        if (response.ok) {
+            alert("Producto agregado exitosamente.");
+            //location.href = "inventario.html";
+        } else {
+            throw new Error("Error al agregar el producto.");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+async function obtenerProductos() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    console.log(id);
+    try{
+        const response = await fetch(`http://127.0.0.1:8000/productos/?id=${id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Token ${cookie}` 
+            },
+    
+        });
+        if(!response.ok){
+            throw new Error('No se pudo obtener la lista')
+        }
+        const data = await response.json();
+        producto = data
+        console.log(producto);
+        document.getElementById("nombre").value = producto.nombre;
+        document.getElementById("precio").value = producto.precio;
+        document.getElementById("cantidad").value = producto.cantidad;
+        document.getElementById("categoriaSelect").value = producto.categoria;
+        document.getElementById("descripcion").value = producto.descripcion;
+        document.getElementById("material").value = producto.material;
+    }catch(error){
+        console.error('Error al obtener productos:', error);
+    }
+        
+}
